e4c55233b826a7ef34b766a29a919ecfffca779c
Ksenya <ksyuhacr7@gmail.com>
2023-02-20 20:47:46 +0300

    add some changes

--- 1st homework/exer4.py
+++ 1st homework/exer4.py
@@ -1,7 +1,10 @@
 S = 24
-Petya = S / 6
-Serezha = Petya
-Katya = (Petya + Serezha) * 2
-print(int(Petya))
-print(int(Katya))
-print(int(Serezha))
+if S % 6 == 0:
+    Petya = S / 6
+    Serezha = Petya
+    Katya = (Petya + Serezha) * 2
+    print(int(Petya))
+    print(int(Katya))
+    print(int(Serezha))
+else:
+    print("Невозможно решить задачу, используя введенное число")
--- 1st homework/exer8.py
+++ 1st homework/exer8.py
@@ -1,7 +1,7 @@
 n = int(input("Введите число n: "))
 m = int(input("Введите число m: "))
 k = int(input("Введите число k: "))
-if k%n == 0 or k%m == 0:
+if (k%n == 0 or k%m == 0) and k>0 and n*m>k:
     print(f"Можно отломить {k} долек")
 else:
     print(f"Нельзя отломить {k} долек")
