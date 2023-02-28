a0aaaa5ba0c46d2d3d15b81bb0994f803f4c393f
Ksenya <ksyuhacr7@gmail.com>
2023-02-28 17:06:37 +0300

    exer 22 is done

---
+++ 4th_homework/exer22.py
@@ -0,0 +1,14 @@
+n = int(input("Введите значение n "))
+m = int(input("Введите значение m "))
+set1 = set()
+set2 = set()
+for i in range(1,n+1):
+    i = int(input())
+    set1.add(i)
+print(set1)
+for i in range(1,m+1):
+    i = int(input())
+    set2.add(i)
+print(set2)
+inner = list(set1.intersection(set2))
+print(sorted(inner))
