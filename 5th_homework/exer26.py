87b5d453d5f9bc9357255eed89305e4d791b76e0
Ksenya <ksyuhacr7@gmail.com>
2023-03-05 11:27:58 +0300

    add exer 26

---
+++ 5th_homework/exer26.py
@@ -0,0 +1,8 @@
+A = int(input("Введите число "))
+B = int(input("Введите степень числа "))
+def num_degree(a,b):
+    if b == 1:
+        return a
+    return a * num_degree(a,b-1)
+
+num_degree(A,B)
