ff929a8f125e378d896010b1c21e4bd321180982
Ksenya <ksyuhacr7@gmail.com>
2023-03-14 18:43:42 +0300

    changed letters

--- 7th_homework/exer34.py
+++ 7th_homework/exer34.py
@@ -1,6 +1,6 @@
 poem_str = "пара-ра-рам рам-пам-папам па-ра-па-да"
 poem_lst = poem_str.split()
-rhyme = list(map(lambda x: x.count('а'), poem_lst))
+rhyme = list(map(lambda x: sum(1 for i in x if i in "аеёиоуыэюя"), poem_lst))
 if len(set(rhyme)) == 1:
     print("Парам пам-пам")
 else:
